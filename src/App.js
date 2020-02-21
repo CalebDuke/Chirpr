b3c0a90857f57e18a5b65033858b9082a7b56fe9
Caleb Duke <dukecj18@gmail.com>
2020-02-21 16:58:32 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      textvalue: ""
+    };
+  }
+
+  handleChirp(e) {
+    this.setState({ textvalue: e.target.value });
+  }
+  //class components need a render method wrapping the return
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={e => this.handleChirp(e)}
+        />
+        <h1>{this.state.textvalue}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
